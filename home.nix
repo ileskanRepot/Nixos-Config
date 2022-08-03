5e6b616e406b88dc39fc75a92374af324542829d
Ileska <ileska@tutanota.com>
2022-08-03 19:28:44 +0300

    Add firefox and chromium config

--- home.nix
+++ home.nix
@@ -5,5 +5,11 @@
     ./i3.nix
     ./bash.nix
     ./vim.nix
+    ./chromium.nix
+    ./firefox.nix
   ];
+  home.sessionVariables = {
+    EDITOR = "vim";
+    NIX_SHELL_PRESERVE_PROMPT=1;
+  };
 }
